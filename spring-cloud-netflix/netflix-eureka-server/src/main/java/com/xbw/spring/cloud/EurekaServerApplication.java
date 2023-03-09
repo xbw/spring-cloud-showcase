a856e7bb71a2a8966bfe14ed3d728c8d3f062777
おう <15621624+xbw@users.noreply.github.com>
2023-03-09 12:28:21 +0800

    Add Eureka 1.x

---
+++ spring-cloud-netflix/netflix-eureka-server/src/main/java/com/xbw/spring/cloud/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.xbw.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author xbw
+ * @see com.netflix.eureka.registry.PeerAwareInstanceRegistryImpl#register
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
