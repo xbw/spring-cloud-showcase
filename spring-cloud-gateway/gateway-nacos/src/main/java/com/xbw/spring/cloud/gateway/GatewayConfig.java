e34075ee06587c44b4aae3c3916ffbb335a8b031
おう <15621624+xbw@users.noreply.github.com>
2023-03-16 15:04:58 +0800

    Add Gateway filter CircuitBreaker

--- spring-cloud-gateway/gateway-nacos/src/main/java/com/xbw/spring/cloud/gateway/GatewayConfig.java
+++ spring-cloud-gateway/gateway-nacos/src/main/java/com/xbw/spring/cloud/gateway/GatewayConfig.java
@@ -1,5 +1,10 @@
 package com.xbw.spring.cloud.gateway;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
@@ -7,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.Objects;
 
 /**
@@ -32,4 +38,17 @@ public class GatewayConfig {
     KeyResolver userKeyResolver() {
         return exchange -> Mono.just(Objects.requireNonNull(exchange.getRequest().getRemoteAddress()).getAddress().getHostAddress());
     }
+
+    /**
+     * @see org.springframework.cloud.gateway.config.GatewayResilience4JCircuitBreakerAutoConfiguration
+     * @return
+     */
+    @Bean
+    public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+                .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+                .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build()).build());
+    }
+
+
 }
